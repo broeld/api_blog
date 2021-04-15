bb19e6d1e0d1d7954e443da1e5d2e05d604d0aa8
broeld <dinaiovcheva@gmail.com>
2021-04-15 17:58:41 +0300

    added: post app and post model

---
+++ apps/posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField(max_length=255)
+    creation_date = models.DateField(auto_now=True)
+    amount_of_upvotes = models.PositiveIntegerField(default=0)
+    user_upvotes = models.ManyToManyField(User,
+                                          blank=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
+
+    class Meta:
+        ordering = ('-creation_date', )
+
+    def __str__(self):
+        return self.title
