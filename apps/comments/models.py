30bcdd1d2745ee7ab4ada73f4318f660123b1380
broeld <dinaiovcheva@gmail.com>
2021-04-15 20:36:01 +0300

    feat: add comment model

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
+    post = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    content = models.TextField()
+    creation_date = models.DateField(auto_now=True)
+
+    parent = models.ForeignKey('self',
+                               on_delete=models.CASCADE,
+                               related_name='replies',
+                               blank=True)
+
+    class Meta:
+        ordering = ('creation_date',)
+
+    def __str__(self):
+        return self.content
