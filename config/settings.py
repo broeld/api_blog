3b7e32abc9d9b852db4f749440d664a3fb9987ed
broeld <dinaiovcheva@gmail.com>
2021-04-15 20:19:04 +0300

    feat: add reset post upvotes once a day

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.users',
-    'apps.posts',
+    'apps.users.apps.UsersConfig',
+    'apps.posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
