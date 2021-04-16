5fd8969eb8f3d09f38143bcd5b18e29b92d588d7
broeld <dinaiovcheva@gmail.com>
2021-04-16 12:18:13 +0300

    feat: add database configuration + migrations

--- apps/comments/serializers.py
+++ apps/comments/serializers.py
@@ -5,11 +5,10 @@ from .models import Comment
 
 class CommentSerializer(ModelSerializer):
     author = ReadOnlyField(source="author.username")
-    replies = ReadOnlyField()
     post = ReadOnlyField(source="post.id")
     parent = ReadOnlyField(source="parent.id", required=False)
 
     class Meta:
         model = Comment
-        fields = ["id", "author", "content", "creation_date",
-                  "post", "replies", "parent"]
+        fields = ["id", "author", "content",
+                  "creation_date", "post", "parent"]
